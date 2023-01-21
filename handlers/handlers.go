7e49a2b224f321c996512b46d8345599d5c9c71c
Ozawa Shuhei <ozawa_shuhei@cyberagent.co.jp>
2023-01-21 20:56:17 +0900

    1-5

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -18,8 +21,13 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Article List")
 }
 
+// GET /article/{id} のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
