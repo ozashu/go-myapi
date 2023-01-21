5f03deeb12ccc4f622b410c758351167984f4716
Ozawa Shuhei <ozawa_shuhei@cyberagent.co.jp>
2023-01-21 21:13:49 +0900

    1-6

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -17,8 +17,23 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Article...\n")
 }
 
+// GET /article/list のハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List")
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 // GET /article/{id} のハンドラ
