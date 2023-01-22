32ee6db1dbb3ff1e45b6e8eafa17f25b95f29a33
Ozawa Shuhei <ozawa_shuhei@cyberagent.co.jp>
2023-01-22 18:34:42 +0900

    2-6

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -31,7 +31,13 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	}
 	defer req.Body.Close()
 
-	article := models.Article1
+	var reqArticle models.Article
+	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+
+	article := reqArticle
 	jsonData, err := json.Marshal(article)
 	if err != nil {
 		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
